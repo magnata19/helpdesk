bf089fafaf51575299060246fc27b44a7d0b012f
dave <dehpacifico@gmail.com>
2025-11-10 22:17:40 -0300

    criando tabela

--- front/helpdesk/src/app/app.module.ts
+++ front/helpdesk/src/app/app.module.ts
@@ -25,16 +25,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 //Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    TecnicoListComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +61,8 @@ import { HomeComponent } from './components/home/home.component';
     MatInputModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Bruno Silva',
+      cpf: '123.456.789-00',
+      email: 'bruno.silva@example.com',
+      senha: 'senha123',
+      perfis: ['0'],
+      dataCriacao: '10/10/2020'
+    }
+  ]
+
+  constructor() { }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
