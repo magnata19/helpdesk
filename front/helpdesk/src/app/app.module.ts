11773a35107d5ae71cd7733024d33155dfd6e29b
dave <dehpacifico@gmail.com>
2025-11-12 14:31:38 -0300

    adicionando toaster

--- front/helpdesk/src/app/app.module.ts
+++ front/helpdesk/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,12 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
